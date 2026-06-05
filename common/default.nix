825adec164f7b6b3eac109d854ebb8f1efc10e12
jug <grabitzkyjulian@gmail.com>
2026-06-05 14:35:06 +0200

    mend

--- common/default.nix
+++ common/default.nix
@@ -25,6 +25,9 @@
     "flakes"
   ];
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
+  ];
 
   environment.systemPackages = with pkgs; [
     vim
