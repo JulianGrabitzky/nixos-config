79fed0877311e35812a37bf624a268cc443bc5c9
jug <grabitzkyjulian@gmail.com>
2026-06-26 13:50:51 +0200

    mend

--- common/default.nix
+++ common/default.nix
@@ -25,6 +25,9 @@
     "flakes"
   ];
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
+  ];
   nixpkgs.overlays = [
     (final: prev: {
       bitwarden-desktop = prev.bitwarden-desktop.override {
